94a9cb5a2cac424c2c88b69d48083dd6898a02e8
dikey <DikeyWang@163.com>
2018-11-20 09:25:46 +0800

    2018.11.20 新增功能：两种查询方式

---
+++ CheckData/src/main/java/com/xtrj/Utils/PropertiesUtil.java
@@ -0,0 +1,33 @@
+package com.xtrj.Utils;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @deprecated 从recourse获取配置信息的工具类
+ * @author dikey
+ * @version 1.0
+ * */
+public class PropertiesUtil {
+    public static Properties properties = null;
+    static{
+        try {
+            // 加载配置属性
+            InputStream inputStream = ClassLoader.getSystemResourceAsStream("kafka.properties");
+            properties = new Properties();
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param key 需要获取的属性名
+     * @return 配置内容,string
+     * */
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
